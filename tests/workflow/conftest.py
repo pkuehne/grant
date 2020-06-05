6ae1d6921d83b3ff609770dd2432c03dbe994a93
Peter Kuehne <Peter.Kuehne@Gmail.com>
2020-06-05 17:46:23 +0100

    Move boilerplate into test class

---
+++ tests/workflow/conftest.py
@@ -0,0 +1,114 @@
+""" Common test fixtures """
+
+import pytest
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QItemSelectionModel
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QTreeView
+from grant.windows.main_window import MainWindow
+from grant.windows.result_dialog import ResultDialog
+
+
+class WindowDriver:
+    """ Test class automating common steps """
+
+    def __init__(self, qtbot):
+        self.window = MainWindow()
+        self.qtbot = qtbot
+
+    def project(self):
+        """ Returns a reference to the project """
+        return self.window.project_manager.project
+
+    def _plan_index(self, row: int):
+        """ Returns QModelIndex for given Plan """
+        return self.window.data_context.data_model.index(
+            row, 0, self.window.data_context.data_model.plans_index
+        )
+
+    def _task_index(self, row: int, plan_index):
+        """ Returns QModelIndex for given Task within given Plan """
+        return self.window.data_context.data_model.index(row, 0, plan_index)
+
+    def create_new_project(self, filename):
+        """ Creates a new project """
+        self.window.menu_bar.file_create_new_action.trigger()
+        assert self.window.project_manager.project.filename == filename
+        assert (
+            self.window.main_screen.selection_stack.currentWidget()
+            == self.window.main_screen.screens["tree"]
+        )
+
+    def add_plan(self):
+        """ Adds a new plan """
+        previous_plan_count = len(self.window.project_manager.project.plans)
+        self.qtbot.mouseClick(
+            self.window.main_screen.screens["tree"].button_add_plan, Qt.LeftButton
+        )
+        new_plan_count = len(self.window.project_manager.project.plans)
+        assert new_plan_count == previous_plan_count + 1
+
+    def select_plan(self, plan_id: int):
+        """ Select a plan in the tree view """
+        tree_view: QTreeView = self.window.main_screen.screens["tree"].tree_view
+        tree_view.expandAll()
+        tree_view.selectionModel().select(
+            self._plan_index(plan_id), QItemSelectionModel.ClearAndSelect,
+        )
+        assert (
+            self.window.main_screen.detail_stack.currentWidget()
+            == self.window.main_screen.screens["plan"]
+        )
+
+    def select_task(self, plan_id: int, task_id: int):
+        """" Select a task in the tree view """
+        tree_view: QTreeView = self.window.main_screen.screens["tree"].tree_view
+        tree_view.selectionModel().select(
+            self._task_index(task_id, self._plan_index(plan_id)),
+            QItemSelectionModel.ClearAndSelect,
+        )
+        assert (
+            self.window.main_screen.detail_stack.currentWidget()
+            == self.window.main_screen.screens["task"]
+        )
+
+    def add_task(self, plan_id: int):
+        """ Adds a task to the plan """
+        previous_task_count = len(
+            self.window.project_manager.project.plans[plan_id].tasks
+        )
+        self.qtbot.mouseClick(
+            self.window.main_screen.screens["tree"].button_add_task, Qt.LeftButton
+        )
+        new_task_count = len(self.window.project_manager.project.plans[plan_id].tasks)
+        assert new_task_count == previous_task_count + 1
+
+    def press_button_in_result_widget(self, button: str, callback=None):
+        """ Pressed the nil button in the result widget """
+        assert (
+            self.window.main_screen.detail_stack.currentWidget()
+            == self.window.main_screen.screens["task"]
+        )
+        # Press "OK" button on ResultDialog
+        def handle_press_ok():
+            if callback is not None:
+                callback()
+            self.qtbot.mouseClick(
+                ResultDialog.dialog_reference.ok_button, Qt.LeftButton
+            )
+
+        # Press "Nil" button in result widget
+        QTimer.singleShot(50, handle_press_ok)
+        self.qtbot.mouseClick(
+            getattr(self.window.main_screen.screens["task"].result, button),
+            Qt.LeftButton,
+        )
+
+
+@pytest.fixture
+def window_driver(qtbot):
+    """ Create a main_window """
+    driver = WindowDriver(qtbot)
+    qtbot.addWidget(driver.window)
+    driver.window.show()
+    return driver
--- tests/workflow/test_task_result.py
+++ tests/workflow/test_task_result.py
@@ -1,114 +1,45 @@
-""" Integration test for Research Results """
+""" Workflow test for Research Results """
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtCore import QItemSelectionModel
-from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QFileDialog
-from PyQt5.QtWidgets import QTreeView
-from grant.windows.main_window import MainWindow
-from grant.windows.result_dialog import ResultDialog
 
 
-def test_research_result(qtbot, monkeypatch, tmpdir):
+def test_research_result(window_driver, monkeypatch, tmpdir):
     """
     Test Schedule:
     * Create Main Window
     * Create new project
     * Add a Plan
-    * Add three tasks
+    * Add two tasks
     * Make task 1 nil result
     * Make task 2 success result
     """
 
     # Setup
-    window = MainWindow()
-    qtbot.addWidget(window)
-
     filename = tmpdir.join("new_project.gra").ensure()
     monkeypatch.setattr(
         QFileDialog, "getSaveFileName", lambda _, __, ___, ____: (str(filename), False)
     )
 
-    def plan_index(row: int):
-        return window.data_context.data_model.index(
-            row, 0, window.data_context.data_model.plans_index
-        )
-
-    def task_index(row: int, plan_index):
-        return window.data_context.data_model.index(row, 0, plan_index)
-
-    # Begin
-    window.show()
-
     # Create new project
-    window.menu_bar.file_create_new_action.trigger()
-    assert window.project_manager.project.filename == filename
-    assert (
-        window.main_screen.selection_stack.currentWidget()
-        == window.main_screen.screens["tree"]
-    )
+    window_driver.create_new_project(filename)
 
-    # Add a new plan
-    qtbot.mouseClick(window.main_screen.screens["tree"].button_add_plan, Qt.LeftButton)
-    assert len(window.project_manager.project.plans) == 1
+    # Add a plan
+    window_driver.add_plan()
 
-    # Expand all nodes
-    tree_view: QTreeView = window.main_screen.screens["tree"].tree_view
-    tree_view.expandAll()
+    # Add a task
+    window_driver.select_plan(0)
+    window_driver.add_task(0)
+    window_driver.select_task(0, 0)
 
-    tree_view.selectionModel().select(
-        plan_index(0), QItemSelectionModel.ClearAndSelect,
-    )
+    # Make result nil
+    window_driver.press_button_in_result_widget("result_nil")
+    assert window_driver.project().plans[0].tasks[0].result.is_nil()
 
-    # Add first task
-    qtbot.mouseClick(window.main_screen.screens["tree"].button_add_task, Qt.LeftButton)
-    assert len(window.project_manager.project.plans[0].tasks) == 1
+    # Add a second task
+    window_driver.select_plan(0)
+    window_driver.add_task(0)
+    window_driver.select_task(0, 1)
 
-    # Select task
-    tree_view.selectionModel().select(
-        task_index(0, plan_index(0)), QItemSelectionModel.ClearAndSelect,
-    )
-    assert (
-        window.main_screen.detail_stack.currentWidget()
-        == window.main_screen.screens["task"]
-    )
-
-    # Press "OK" button on ResultDialog
-    def handle_press_ok():
-        qtbot.mouseClick(ResultDialog.dialog_reference.ok_button, Qt.LeftButton)
-
-    # Press "Nil" button in result widget
-    QTimer.singleShot(50, handle_press_ok)
-    qtbot.mouseClick(
-        window.main_screen.screens["task"].result.result_nil, Qt.LeftButton
-    )
-
-    # Check the result is now nil
-    assert window.project_manager.project.plans[0].tasks[0].result.is_nil()
-
-    # Select plan
-    tree_view.selectionModel().select(
-        plan_index(0), QItemSelectionModel.ClearAndSelect,
-    )
-
-    # Add second task
-    qtbot.mouseClick(window.main_screen.screens["tree"].button_add_task, Qt.LeftButton)
-    assert len(window.project_manager.project.plans[0].tasks) == 2
-
-    # Select task
-    tree_view.selectionModel().select(
-        task_index(1, plan_index(0)), QItemSelectionModel.ClearAndSelect,
-    )
-    assert (
-        window.main_screen.detail_stack.currentWidget()
-        == window.main_screen.screens["task"]
-    )
-
-    # Press "Success" button in result widget
-    QTimer.singleShot(50, handle_press_ok)
-    qtbot.mouseClick(
-        window.main_screen.screens["task"].result.result_success, Qt.LeftButton
-    )
-
-    # Check the result is now nil
-    assert window.project_manager.project.plans[0].tasks[1].result.is_nil() is False
+    # Make result success
+    window_driver.press_button_in_result_widget("result_success")
+    assert not window_driver.project().plans[0].tasks[1].result.is_nil()
