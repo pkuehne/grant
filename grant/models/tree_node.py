6361d3958c6ccdbefca18672ac05a89b2425b563
Peter Kuehne <peter.kuehne@gmail.com>
2021-10-08 18:37:59 +0100

    Don't rebuild whole model on add. Fixes #33

--- grant/models/tree_node.py
+++ grant/models/tree_node.py
@@ -5,7 +5,7 @@ from PyQt5.QtGui import QIcon
 
 
 class TreeNode:
-    """ A wrapper class to normalize the parent/child relationship for node items """
+    """A wrapper class to normalize the parent/child relationship for node items"""
 
     def __init__(self, node_type, data, parent, row):
         self.type = node_type
@@ -15,7 +15,7 @@ class TreeNode:
         self.children = self.get_children()
 
     def get_children(self):
-        """ Return the sub-items (plans/tasks/etc) for the given node """
+        """Return the sub-items (plans/tasks/etc) for the given node"""
         if self.type == "plans":
             return [
                 TreeNode("plan", plan, self, index)
@@ -29,7 +29,7 @@ class TreeNode:
         return []
 
     def delete_child(self, index):
-        """ Delete index from children """
+        """Delete index from children"""
         if self.type == "plans":
             self.data.delete_plan(index)
             del self.children[index]
@@ -38,15 +38,16 @@ class TreeNode:
             del self.children[index]
 
     def create_child(self):
-        """ Creates a new child depending on the type """
+        """Creates a new child depending on the type"""
         if self.type == "plans":
-            self.data.add_plan()
+            plan = self.data.add_plan()
+            self.children.append(TreeNode("plan", plan, self, len(self.children)))
         if self.type == "plan":
-            self.data.add_task()
-        self.children = self.get_children()
+            task = self.data.add_task()
+            self.children.append(TreeNode("task", task, self, len(self.children)))
 
     def get_text(self):
-        """ Return a stringified representation for the given node """
+        """Return a stringified representation for the given node"""
         if self.type == "gedcom":
             return "No gedcom file linked" if self.data == "" else self.data
         if self.type == "filename":
@@ -60,14 +61,14 @@ class TreeNode:
         return ""
 
     def set_text(self, value):
-        """ Updates the text property of the node """
+        """Updates the text property of the node"""
         if self.type == "plan":
             self.data.ancestor = value
         if self.type == "task":
             self.data.source = value
 
     def get_description(self):
-        """ Return a description for the given node """
+        """Return a description for the given node"""
         if self.type == "plan":
             return self.data.goal
         if self.type == "task":
@@ -75,31 +76,31 @@ class TreeNode:
         return ""
 
     def set_description(self, value):
-        """ Updates the description property of the node """
+        """Updates the description property of the node"""
         if self.type == "plan":
             self.data.goal = value
         if self.type == "task":
             self.data.description = value
 
     def get_result(self):
-        """ Return the value for the result """
+        """Return the value for the result"""
         if self.type != "task":
             return ""
         return self.data.result
 
     def set_result(self, value):
-        """ Updates the result for a task """
+        """Updates the result for a task"""
         if self.type == "task":
             self.data.result = value
 
     def get_ancestor(self):
-        """ Return the parent plan's ancestor value """
+        """Return the parent plan's ancestor value"""
         if self.type != "task":
             return ""
         return self.parent.data.ancestor
 
     def get_link(self):
-        """ Returns the node's gedcom link """
+        """Returns the node's gedcom link"""
         if self.type == "task":
             return self.data.source_link
         if self.type == "plan":
@@ -107,14 +108,14 @@ class TreeNode:
         return ""
 
     def set_link(self, value):
-        """ Sets the node's gedcom link to value """
+        """Sets the node's gedcom link to value"""
         if self.type == "task":
             self.data.source_link = value
         if self.type == "plan":
             self.data.ancestor_link = value
 
     def get_icon(self):
-        """ Returns a QIcon for this node """
+        """Returns a QIcon for this node"""
         if self.type == "plan":
             return QIcon(":/icons/plan.ico")
         if self.type == "task":
@@ -122,14 +123,14 @@ class TreeNode:
         return QIcon()
 
     def get_font(self):
-        """ Returns the font to display the item in """
+        """Returns the font to display the item in"""
         font = QFont()
         if self.is_complete():
             font.setStrikeOut(True)
         return font
 
     def is_complete(self):
-        """ Whether this plan/task is complete or not """
+        """Whether this plan/task is complete or not"""
         if self.type == "plans":
             return False
         if self.type == "task":
